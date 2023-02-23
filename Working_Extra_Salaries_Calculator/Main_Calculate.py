72fe57d287b504b2203955abf04e928291bdfeb6
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2023-02-23 22:43:55 +0100

    Add files via upload

---
+++ Working_Extra_Salaries_Calculator/Main_Calculate.py
@@ -0,0 +1,17 @@
+''' Goal of the Code
+1. Able to input Data such as Tax and salaries
+2. Put amount of Hours
+3. Get the salaries before and after taxes
+4. Optional: Save as a CSV to use later 
+'''
+
+import pandas as pd
+import numpy as np
+
+def tax(taxs):
+
+    return taxs
+
+result = tax(5)
+
+
