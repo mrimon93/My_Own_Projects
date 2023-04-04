b87409bc92f171a2d9a748505eed483d059fc099
Muzammil K <118689925+mrimon93@users.noreply.github.com>
2023-04-04 12:10:36 +0200

    Update Download_Data_By_Api.py

--- Format_Code_For_Lazy_People/Download_Data_By_Api.py
+++ Format_Code_For_Lazy_People/Download_Data_By_Api.py
@@ -12,13 +12,50 @@ import requests
 import pandas as pd
 import numpy as np
 
+#Function Files
+def Json_File():
+    # Save To Json File
+    name = input('Name of the file? ')
+    df.to_json(name)
+def CSV_File():
+    # Save to CSV File
+    name = input('Name of the file? ')
+    df.to_csv(name, index=False)
+def Excel_File():
+    # Save to Excel File
+    name = input('Name of the file? ')
+    df.to_excel(name, index=False)
 
 
 #Get the API key and then execute one of those
-url = ""
+# An example: url = "https://api.publicapis.org/entries"
 
+#Enter Your Api Key
+url=""
 
 
 # Getting the Response of the Link
 response = requests.get(url)
-print(response)
+print('The Response of the website is', response)
+
+# Getting in to pandas
+
+data = response.json()
+
+df = pd.DataFrame(data)
+
+print('Which file do you want to save in?')
+print('Press 1 For Json-File')
+print('Press 2 for CSV File')
+print('Press 3 for Excel File')
+
+choice = int(input('Choose your Number; '))
+
+if choice == 1:
+    Json_File()
+elif choice == 2:
+    CSV_File()
+elif choice == 3:
+    Excel_File()
+else:
+    print('You Suck! ')
